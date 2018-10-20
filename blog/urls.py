316412dc0191ce07cfc8dd3c41a465b303656f18
Javier <javier.alexander335@outlook.com>
2018-10-20 14:23:12 -0300

    Agrego el HTML de galeria, junto a su respectivo css. Ademas, implementando la coenxion desde index a galery

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('inicio', views.post_list, name='post_list'),
+    path('photos', views.galery, name='galery'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,8 @@
-
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('photos', include('blog.urls')),
 ]
